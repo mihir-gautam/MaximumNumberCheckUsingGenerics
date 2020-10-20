65f2d2ce6592b133ef69a1b07ea7ed9494992375
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-21 01:59:38 +0530

    [Mihir] Refactor Generic method for test cases added

--- MaximumNumberCheck/GenericMaximum.cs
+++ MaximumNumberCheck/GenericMaximum.cs
@@ -6,6 +6,36 @@ namespace MaximumNumberCheck
 {
     public class GenericMaximum<T> where T : IComparable
     {
+        //UC3
+        public T firstValue, secondValue, thirdValue;
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
         public T[] value;
         public GenericMaximum(T[] value)
         {
@@ -21,7 +51,6 @@ namespace MaximumNumberCheck
         {
 
         }
-        //UC4 
         public T MaxValue(params T[] values)
         {
             var sorted_values = Sort(values);
@@ -29,7 +58,6 @@ namespace MaximumNumberCheck
             return sorted_values[sorted_values.Length - 1];
         }
 
-        //UC5
         public T MaxMethod()
         {
             var max = MaxValue(this.value);
