561e2265d53df507d6bcc680323672b9284491a0
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-20 15:34:00 +0530

    [Mihir] Add Welcome message

---
+++ MaximumNumberCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaximumNumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum number check program.");
+        }
+    }
+}
