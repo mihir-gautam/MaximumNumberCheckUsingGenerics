af51a61c98673d07ae718d0442963a300050b99c
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-20 15:41:32 +0530

    [Mihir] Add Method to find maximum of 3 integers

--- MaximumNumberCheck/Program.cs
+++ MaximumNumberCheck/Program.cs
@@ -7,6 +7,9 @@ namespace MaximumNumberCheck
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum number check program.");
+            Console.WriteLine("Find max Number");
+            int output = MaxNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
         }
     }
 }
