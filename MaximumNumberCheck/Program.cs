c3f952a0bb63f7c1d2e224207d2cd81b3357a658
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-20 16:29:48 +0530

    [Mihir] Add Maximum of 3 strings method

--- MaximumNumberCheck/Program.cs
+++ MaximumNumberCheck/Program.cs
@@ -8,11 +8,13 @@ namespace MaximumNumberCheck
         {
             Console.WriteLine("Welcome to Maximum number check program.");
             Console.WriteLine("Find max Number");
-            MaxNumberCheck MaxInt = new MaxNumberCheck();
-            int output = MaxInt.MaximumIntegerNumber(11,22,33);
+            MaxNumberCheck MaxNum = new MaxNumberCheck();
+            int output = MaxNum.MaximumIntegerNumber(11,22,33);
             Console.WriteLine(output);
-            float floatoutput = MaxInt.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
+            float floatoutput = MaxNum.MaximumFloatNumber(111.2f, 22.45f, 78.56f);
             Console.WriteLine(floatoutput);
+            string stringoutput = MaxNum.MaximumStringNumber("22", "33", "44");
+            Console.WriteLine(stringoutput);
         }
     }
 }
